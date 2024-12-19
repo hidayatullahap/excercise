2b5aa7dbe707ac16b05da027e2c5a6e5b5045e83
Hidayatullah Agung <hidayatullahap@gmail.com>
2024-12-19 17:24:51 +0700

    join

--- palindrome.ts
+++ palindrome.ts
@@ -8,5 +8,15 @@
  */
 export function isPalindrome(word: string): boolean {
     const drow = word.split("").reverse().join("")
-    return word === drow;
+    return word === drow
+}
+
+export function isPalindromeV2(word: string): boolean {
+    const drow: string[] = []
+    const split = word.split("")
+    for (let i = split.length; i >= 0; i--) {
+        drow.push(split[i])
+    }
+
+    return word === drow.join("")
 }
--- test.ts
+++ test.ts
@@ -1,19 +1,28 @@
 import { describe, it } from "@std/testing/bdd";
 import { expect } from "@std/expect";
-import { isPalindrome } from "./palindrome.ts";
+import { isPalindrome, isPalindromeV2 } from "./palindrome.ts";
+
+const testCases = [
+    { word: "kayak", expected: true },
+    { word: "racecar", expected: true },
+    { word: "rotator", expected: true },
+    { word: "wow", expected: true },
+    { word: "hello", expected: false },
+];
 
 describe("isPalindrome", () => {
-    const testCases = [
-        { word: "kayak", expected: true },
-        { word: "racecar", expected: true },
-        { word: "rotator", expected: true },
-        { word: "wow", expected: true },
-        { word: "hello", expected: false },
-    ];
-
     for (const testCase of testCases) {
         it(`should return ${testCase.expected} for ${testCase.word}`, () => {
             const result = isPalindrome(testCase.word);
+            expect(result).toBe(testCase.expected);
+        });
+    }
+});
+
+describe("isPalindrome2", () => {
+    for (const testCase of testCases) {
+        it(`should return ${testCase.expected} for ${testCase.word}`, () => {
+            const result = isPalindromeV2(testCase.word);
             expect(result).toBe(testCase.expected);
         });
     }
